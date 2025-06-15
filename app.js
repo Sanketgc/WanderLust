5bf3a9889ae422ac797528bdbf2bb6ddaa1577ec
Sanket <sanketchanagond07@gmail.com>
2025-06-15 21:47:35 +0530

    Image upload

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+if(process.env.NODE_ENV != "production"){
+    require('dotenv').config(); 
+}
+console.log(process.env);
+
 const express = require("express");
 const app =express();
 const mongoose = require("mongoose");
@@ -10,7 +15,8 @@ const session = require("express-session");
 const flash =require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User = require("./models/user.js")
+const User = require("./models/user.js");
+
 
 const listingrouter = require("./Routes/listing.js");
 const reviewRouter = require("./Routes/review.js");
@@ -30,7 +36,7 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
